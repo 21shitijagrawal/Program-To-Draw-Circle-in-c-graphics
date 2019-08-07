2f29b6f4ebd94b569dcae73841e708defcfd5ebf
21shitijagrawal <44740279+21shitijagrawal@users.noreply.github.com>
2019-08-07 16:32:34 +0530

    Update and rename CIR.C to Happy_FriendShip_Day.C

--- Example/Happy_FriendShip_Day.C
+++ Example/Happy_FriendShip_Day.C
@@ -2,62 +2,8 @@
 #include<conio.h>
 #include<math.h>
 #include<graphics.h>
-void drawCircle(int x1, int y1,int r){
-     int angle=0,x,y;
-     int xc=x1/2,yc=y1/2;
-     while(angle<360){
-         x=cos(angle)*r;
-         y=sin(angle)*r;
-         if(x>0){
-            x=xc+x;
-         }
-         else{
-            x=xc+x;
-         }
-         if(y>0){
-            y=yc+y;
-         }
-         else{
-            y=yc+y;
-         }
-         putpixel(x,y,15);
-         angle++;
-     }
-}
-void main(){
-     int gd=DETECT,gm;
-     int x,y;
-     clrscr();
-     initgraph(&gd,&gm,"C:\\TC\\BGI");
-     drawCircle(10,20,10);
-     getch();
-     closegraph();
-}
-
-/*
 #include<stdlib.h>
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
-#include<graphics.h>
-void translate(int *x,int *y, int xcord, int ycord){
-     int MAXX=xcord,MAXY=ycord;
-     int xc=MAXX/2,yc=MAXY/2;
-     if(*x>0){
-	 *x=xc+(*x);
-     }
-     else{
-	 *x=xc+(*x);
-     }
-     if(*y>0){
-	 *y=yc+(*y);
-     }
-     else{
-	 *y=yc+(*y);
-     }
-     line(xc,0,xc,MAXY);
-     line(0,yc,MAXX,yc);
-}
+
 void drawCircle(int x1, int y1,int r){
      int angle=0,x,y,c=0,i=0;
      int xc=x1/2,yc=y1/2;
@@ -108,4 +54,3 @@ void main(){
      getch();
      closegraph();
 }
-*/
